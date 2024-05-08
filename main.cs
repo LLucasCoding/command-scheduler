466201b2dfb9d8a5e91b0f0524f81f36a01956c2
LLucas2 <16520649-LLucas2@users.noreply.replit.com>
2024-05-08 20:32:43 +0000

    Each execution removes the first line of data, now the commands are executed in a for loop that is broken once all the commands are run... If the computer shuts down then the line is still not parsed as it is only parsed when it executes

--- main.cs
+++ main.cs
@@ -11,30 +11,39 @@ class Program
 
         Console.WriteLine("Current timestamp: " + timestamp); // print timestamp
 
-        // File reading process
-        string lineread = File.ReadLines("data.txt").First(); // get first line
-        string[] linewords = lineread.Split(); // split file into words
-        
-        int targetstamp = Int32.Parse(linewords[0]); // get target timestamp
-        string command = lineread;
+        short commandcount = (short)(File.ReadAllLines("data.txt").Length);
 
-        int ind = command.IndexOf(" ") + 1; // remove command from line
-        command = command.Substring(ind);
-        
-        Console.WriteLine("Targestamp: " + targetstamp);
-        int timediff = targetstamp - timestamp;
-        Console.WriteLine("Seconds to \"shutdown\": " + timediff); // time difference, seconds
-        if (timediff > 0)
-        {
-            System.Threading.Thread.Sleep(timediff * 1000);
+        for (short command_num = 0; command_num < commandcount; command_num ++) {
+            // File reading process
+            string lineread = File.ReadLines("data.txt").First(); // get first line
+            string[] linewords = lineread.Split(); // split file into words
+    
+            int targetstamp = Int32.Parse(linewords[0]); // get target timestamp
+            string command = lineread;
+    
+            int ind = command.IndexOf(" ") + 1; // remove command from line
+            command = command.Substring(ind);
+    
+            Console.WriteLine("Targestamp: " + targetstamp);
+            int timediff = targetstamp - timestamp;
+            Console.WriteLine("Seconds until command: " + timediff); // time difference, seconds
+            if (timediff > 0)
+            {
+                System.Threading.Thread.Sleep(timediff * 1000);
+            }
+            else
+            {
+                Console.WriteLine("The time was negative, therefore I run the command immediately.");
+            }
+    
+            Console.WriteLine("Removing first line");
+            var lines = File.ReadAllLines("data.txt"); // remove first line of data
+            File.WriteAllLines("data.txt", lines.Skip(1).ToArray());
+            
+            Console.WriteLine("Running command! The command was");
+            Console.WriteLine(command);
         }
-        else
-        {
-            Console.WriteLine("The time was negative, therefore I shut down no matter what");
-        }
-        Console.WriteLine("Running command! The command was");
-        Console.WriteLine(command);
-        
+
         return;
     }
 }
